bce715e34e8bbe98b12e1b14882a6706d1476891
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2026-03-20 16:47:12 -0300

    Update trailer.py

--- src/trailer.py
+++ src/trailer.py
@@ -1,5 +1,2 @@
 def gerar_trailer(sequencia):
-    registro = "9"
-    brancos = " " * 393
-    numero_sequencial = str(sequencia).rjust(6, "0")
-    return (registro + brancos + numero_sequencial)[:400]
+    return "9" + (" " * 393) + str(sequencia).rjust(6, "0")
