94b987226b8652daf31141984a024b39807bee1d
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2026-03-20 17:43:22 -0300

    Update trailer.py

--- src/trailer.py
+++ src/trailer.py
@@ -1,2 +1,7 @@
 def gerar_trailer(sequencia):
-    return "9" + (" " * 393) + str(sequencia).rjust(6, "0")
+    linha = (
+        "9" +
+        " " * 393 +
+        str(sequencia).rjust(6, "0")
+    )
+    return linha
