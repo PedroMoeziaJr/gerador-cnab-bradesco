062f1887887047c1feca4c4c670e7b9e217a4d4a
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2026-03-20 17:22:40 -0300

    Update detalhe.py

--- src/detalhe.py
+++ src/detalhe.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def gerar_detalhe(boleto, sequencia):
     registro = "1"
 
@@ -14,7 +16,7 @@ def gerar_detalhe(boleto, sequencia):
 
     nome_sacado = boleto["nome"].upper().ljust(40)
     endereco = boleto["endereco"].upper().ljust(40)
-    bairro = boleto["bairro"].upper().ljust(15)
+    bairro = boleto["bairro"].upper().ljust(12)
     cep = boleto["cep"].replace("-", "").rjust(8, "0")
     cidade = boleto["cidade"].upper().ljust(15)
     uf = boleto["uf"].upper().ljust(2)
@@ -24,23 +26,24 @@ def gerar_detalhe(boleto, sequencia):
         agencia.rjust(4, "0") +             # 2–5
         conta.rjust(7, "0") +               # 6–12
         conta_dv +                          # 13
-        " " * 25 +                          # 14–38
-        nosso_numero[:10] +                 # 38–47
-        dv_nosso_numero +                   # 48
-        " " * 60 +                          # 49–108
-        carteira +                          # 109–111
-        " " * 10 +                          # 112–121
-        vencimento +                        # 122–127
-        valor +                             # 128–140
-        " " * 7 +                           # 141–147
-        nome_sacado +                       # 148–187
-        endereco +                          # 188–227
-        bairro +                            # 228–242
-        cep +                               # 243–250
-        cidade +                            # 251–265
-        uf +                                # 266–267
-        " " * 127 +                         # 268–394
-        str(sequencia).rjust(6, "0")        # 395–400
+        carteira +                          # 14–16
+        nosso_numero +                      # 17–27
+        dv_nosso_numero +                   # 28
+        " " * 6 +                           # 29–34
+        "0" * 13 +                          # 35–47 (uso do banco)
+        " " * 4 +                           # 48–51
+        vencimento +                        # 52–57
+        valor +                             # 58–70
+        "0" * 3 +                           # 71–73
+        " " * 25 +                          # 74–98
+        nome_sacado +                       # 99–138
+        endereco +                          # 139–178
+        bairro +                            # 179–190
+        cep +                               # 191–198
+        cidade +                            # 199–213
+        uf +                                # 214–215
+        " " * 178 +                         # 216–393
+        str(sequencia).rjust(6, "0")        # 394–399
     )
 
     return linha[:400]
