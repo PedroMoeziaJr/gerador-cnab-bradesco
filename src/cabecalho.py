c5a2c9b3c3c8fd6abcc48203988608a79b2042f6
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2026-03-20 17:42:47 -0300

    Update cabecalho.py

--- src/cabecalho.py
+++ src/cabecalho.py
@@ -1,44 +1,26 @@
 from datetime import datetime
 
 def gerar_cabecalho():
-    registro = "0"
-    tipo_operacao = "1"
-    espaco = " "
-    literal_remessa = "REMESSA".ljust(7)
-    codigo_servico = "01"
-    literal_servico = "COBRANCA".ljust(15)
-
-    agencia = "0606"
-    conta = "1656414"
-    conta_dv = "4"
-
+    codigo_empresa = "0000000007867330"  # 20 posições (convênio alinhado à direita)
     nome_empresa = "PRIME PARK ESTACIONAMENTOS LTDA".ljust(30)
-    codigo_banco = "237"
-    nome_banco = "BRADESCO".ljust(15)
-
     data_hoje = datetime.now().strftime("%d%m%y")
 
-    uso_do_banco = " " * 8
-    complemento = " " * 294
-    numero_sequencial = "000001"
-
     linha = (
-        registro +
-        espaco +               # <<<<<<<<<< AQUI ESTAVA O ERRO
-        tipo_operacao +
-        literal_remessa +
-        codigo_servico +
-        literal_servico +
-        agencia.rjust(4, "0") +
-        conta.rjust(7, "0") +
-        conta_dv +
-        uso_do_banco +
+        "0" +
+        "1" +
+        "REMESSA" +
+        "01" +
+        "COBRANCA".ljust(15) +
+        codigo_empresa +
         nome_empresa +
-        codigo_banco +
-        nome_banco +
+        "237" +
+        "BRADESCO".ljust(15) +
         data_hoje +
-        complemento +
-        numero_sequencial
+        " " * 8 +
+        "MX" +
+        "0000001" +
+        " " * 277 +
+        "000000"
     )
 
-    return linha[:400]
+    return linha
